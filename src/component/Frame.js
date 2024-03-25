a6d23e0db9d2ed93956f25b12e601ef75baddb52
harshita <pathakharshita06@gmail.com>
2024-03-25 13:11:26 +0530

    push

--- src/component/Frame.js
+++ src/component/Frame.js
@@ -4,62 +4,76 @@ import "./Frame.css";
 const Frame = () => {
   return (
     <div className="frame-root">
-      <div className="join-us-for-exciting-container">
-        <div className="join-us-for2">Join us for exciting</div>
-        <div className="header1">
-          <div className="section-heading7">
-            <h1 className="about-us7">Events</h1>
-            <img
-              className="gradient-underline-icon7"
-              loading="lazy"
-              alt=""
-              src="/gradient-underline2.svg"
-            />
-          </div>
+      <div className="call-to-action-container">
+        <div className="image-container">
+          <div className="upper">
           <img
-            className="arrow-right-solid-1-icon2"
+            className="call-to-action"
             loading="lazy"
             alt=""
-            src="/arrowrightsolid-1.svg"
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(35.33% - 10px)', height: '300px' }} // Adjust the width here
+          />
+          <img
+            className="call-to-action1"
+            loading="lazy"
+            alt=""
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(23.33% - 10px)', height: '250px' , margin: "0px 20px"}} // Adjust the width here
+          />
+          <img
+            className="frame-child6"
+            loading="lazy"
+            alt=""
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(41.33% - 10px)', height: '300px' }} // Adjust the width here
+          />
+          </div>
+          <div className="center-div " style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-60%, -70%)' }}>
+          {/* Your content for the center div */}
+          <div className="join-us-for2">Join us for exciting</div>
+          <div className="header1">
+            <div className="section-heading7">
+              <div className="about-us7">Events
+              <div className="underline"></div></div>
+              
+            </div>
+            <img
+              className="arrow-right-solid-1-icon2"
+              loading="lazy"
+              alt=""
+              // src="/arrowrightsolid-1.svg"
+            />
+          </div>
+
+
+        </div>
+          <img
+            className="call-to-action2"
+            loading="lazy"
+            alt=""
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(33.33% - 10px)' , height: '290px'}} // Adjust the width here
+          />
+          <img
+            className="call-to-action3"
+            loading="lazy"
+            alt=""
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(33.33% - 10px)', height: '270px'  , margin: "0px 15px" }} // Adjust the width here
+          />
+          <img
+            className="call-to-action4"
+            loading="lazy"
+            alt=""
+            src={require("../images/Screenshot 2024-03-03 195851.png")}
+            style={{ width: 'calc(33.33% - 10px)', height: '290px' }} // Adjust the width here
           />
         </div>
       </div>
-      <img
-        className="call-to-action"
-        loading="lazy"
-        alt=""
-        src="/rectangle-17@2x.png"
-      />
-      <img
-        className="call-to-action1"
-        loading="lazy"
-        alt=""
-        src="/rectangle-18@2x.png"
-      />
-      <div className="frame8">
-        <img className="frame-child6" alt="" src="/rectangle-20@2x.png" />
-        <div className="frame-child7" />
-      </div>
-      <img
-        className="call-to-action2"
-        loading="lazy"
-        alt=""
-        src="/rectangle-19@2x.png"
-      />
-      <img
-        className="call-to-action3"
-        loading="lazy"
-        alt=""
-        src="/rectangle-21@2x.png"
-      />
-      <img
-        className="call-to-action4"
-        loading="lazy"
-        alt=""
-        src="/rectangle-22@2x.png"
-      />
     </div>
   );
 };
+
 
 export default Frame;
