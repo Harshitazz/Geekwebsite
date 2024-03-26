e9f89a9a90708fad30b1b70cce42bb7dedcb7140
Aditya <adityakumarji74@gmail.com>
2024-03-27 00:09:32 +0530

    contact form done

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,45 +1,39 @@
-import React from "react"
-import "./ContactForm.css"
+import React from "react";
+import "./ContactForm.css";
 
 export default function ContactForm() {
   return (
     <div className="group-3">
-      
       <div className="contact-form-details">
-        <div className="section-heading">
-          <p className="about-us">Contact Form</p>
+        <div className="section-heading ">
+          <p className="about-us ">Contact Form</p>
+          <div className="underline"></div>
         </div>
-        <div className="frame-141">
+        <div className="frame-141 ">
           <div className="frame-13">
+            <div class="sphere1" style={{ left: "80%" }}></div>
             <div className="frame-10">
-              <p className="first-name">First Name</p>
+              <input placeholder="FIRST NAME" className="info" />
             </div>
             <div className="frame-11">
-              <p className="last-name">Last Name</p>
+              <input placeholder="LAST NAME" className="info" />
             </div>
+            <div class="sphere2" style={{ left: "-45%" }}></div>
           </div>
           <div className="frame-12">
-            <p className="email">Email</p>
+            <input placeholder="EMAIL" className="info" />
           </div>
           <div className="frame-14">
-            <p className="message">Message</p>
+            <input placeholder="MESSAGE" className="info" />
           </div>
         </div>
 
-        <img
-        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/i49yjf87qgh-113%3A230?alt=media&token=a88a681e-c0ec-40b3-a046-722bf414557f"
-        alt="Not Found"
-        className="frame"
-      />
-      <p className="submit">Submit</p>
+        <button className="submit">Submit</button>
       </div>
 
-      
-        <div className="group-892">
-          <p className="ask-aquestion">Ask a Question</p>
-        </div>
-      
+      <div className="group-892">
+        <p className="ask-aquestion">Ask a Question</p>
+      </div>
     </div>
-    
-  )
+  );
 }
