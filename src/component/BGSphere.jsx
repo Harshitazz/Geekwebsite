406c89e018868b76f16c7e46de2d36bac01a984c
harshita <pathakharshita06@gmail.com>
2024-04-23 22:00:51 +0530

    push

--- src/component/BGSphere.jsx
+++ src/component/BGSphere.jsx
@@ -1,9 +1,8 @@
+import React from 'react';
 
-
-
-function BGSphere({width, height, x, y, color}){
+function BGSphere({ width, height, x, y, color }) {
    return (
-      <div style={{
+      <div className="bgsphere" style={{
          width: width,
          height: height,
          position: 'absolute',
@@ -17,6 +16,25 @@ function BGSphere({width, height, x, y, color}){
          transform: 'translate(-50%, -50%)'
       }}></div>
    )
-}  
+}
+
+// Use template literals to embed the CSS media query within the JavaScript file
+const styles = `
+  @media (max-width: 700px) {
+    .bgsphere {
+      display:none
+    }
+  }
+`;
+
+// Inject the styles into the document using a <style> tag
+function addStyles() {
+  const styleEl = document.createElement('style');
+  styleEl.appendChild(document.createTextNode(styles));
+  document.head.appendChild(styleEl);
+}
+
+// Call the function to inject styles when the component file is executed
+addStyles();
 
 export default BGSphere;
